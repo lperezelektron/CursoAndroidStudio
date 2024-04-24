7a0583157551214dd3382b4dc163a2a604fe9ca9
Luis Guillermo Pérez Fuentes <lperez@elektron.com.mx>
2024-04-24 09:13:06 -0600

    Se agregan los colores de la línea en el CategoriesViewHolder y se da un pequeño margen

--- app/src/main/java/com/example/androidexercises/ToDoApp/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/androidexercises/ToDoApp/CategoriesViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.androidexercises.ToDoApp
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidexercises.R
 
@@ -11,7 +12,26 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val divider:View = view.findViewById(R.id.divider)
 
     fun render(taskCategory: TaskCategory) {
-
+        when(taskCategory){
+            TaskCategory.Business -> {
+                tvCategoryName.text = ContextCompat.getString(tvCategoryName.context, R.string.business)
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.business_category)
+                )
+            }
+            TaskCategory.Other -> {
+                tvCategoryName.text = ContextCompat.getString(tvCategoryName.context, R.string.others)
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.other_category)
+                )
+            }
+            TaskCategory.Personal -> {
+                tvCategoryName.text = ContextCompat.getString(tvCategoryName.context, R.string.personal)
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.personal_category)
+                )
+            }
+        }
     }
 
 }
